35afc4076d27306efa12406f159d72bca38cd3c4
Amandine Huret <amandine.huret@gmail.com>
2022-02-10 09:05:13 +0100

    AccueilMoche

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { AvisComponent } from './composants/avis/avis.component';
 import { CollegueComponent } from './composants/collegue/collegue.component';
 import { ListeColleguesComponentComponent } from './composants/liste-collegues-component/liste-collegues-component.component';
+import { AccueilComponent } from './composants/accueil/accueil.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AvisComponent,
     CollegueComponent,
-    ListeColleguesComponentComponent
+    ListeColleguesComponentComponent,
+    AccueilComponent
   ],
   imports: [
     BrowserModule
